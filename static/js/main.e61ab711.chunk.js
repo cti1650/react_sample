ba1b3f5f904c22df038c2849e1506c321ef56d14
cti1650 <cti1650@gmail.com>
2021-01-17 15:19:55 +0900

    Updates

---
+++ static/js/main.e61ab711.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptest=this.webpackJsonptest||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(0),s=n(1),r=n.n(s),i=n(3),o=n.n(i);n(9);var d=function(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("p",{children:"Hello World!"})})};o.a.render(Object(c.jsx)(r.a.StrictMode,{children:Object(c.jsx)(d,{})}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e61ab711.chunk.js.map
